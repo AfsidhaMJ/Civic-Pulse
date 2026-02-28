64e2c850bef583ce77ac139db4b2cf6e2c0b97be
Afsidha MJ <mjafsidha@gmail.com>
2026-02-28 09:59:04 +0530

    initial

--- main.js
+++
@@ -1,6 +0,0 @@
-fetch("/api/stats")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("total").innerText = data.total;
-    document.getElementById("submitted").innerText = data.submitted;
-  });
